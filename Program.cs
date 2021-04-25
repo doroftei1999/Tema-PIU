b90a7cb7ed5c97c03bfb9848276f0107ff2e0afb
doroftei1999 <doroftei1999@yahoo.com>
2021-04-25 13:10:31 +0300

    Defined the class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Tema
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            show first = new show();
+            first.name = "The Big Bang Theory";
+            Console.WriteLine(first.name);
+            first.review = 8.1;
         }
+    }
+
+    internal class show
+    {
+        public string name
+        { get; set; }
+
+        public string genre
+        { get; set; }
+
+        public int numberOfEpisodes
+        { get; set; }
+
+        public int numberOfSeasons
+        { get; set; }
+
+        public double review
+        {
+            get { return review; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    review = value;
+                else
+                    review = -1;
+            }
+        }
+
+        public bool onGoing
+        { get; set; }
     }
 }
