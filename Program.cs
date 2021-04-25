74c2e5c0082cc563237b8ec2d6149785e0ae13f3
doroftei1999 <doroftei1999@yahoo.com>
2021-04-25 13:10:31 +0300

    Defined the class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Tema
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            show a = new show();
+            a.name = "dfdfd";
+            Console.WriteLine(a.name);
         }
+    }
+
+    internal class show
+    {
+        public string name
+        { get; set; }
+
+        public string genre
+        { get; set; }
+
+        public int numberOfEpisodes
+        { get; set; }
+
+        public int numberOfSeasons
+        { get; set; }
+
+        public float review
+        { get; set; }
+
+        public bool onGoing
+        { get; set; }
     }
 }
