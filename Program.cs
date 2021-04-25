15e58eef40a17d13690f4a9005bc508bf4397a41
doroftei1999 <doroftei1999@yahoo.com>
2021-04-25 14:30:54 +0300

    Am implementat constructorul cu sir de caractere

--- Program.cs
+++ Program.cs
@@ -6,59 +6,54 @@ namespace Tema
     {
         private static void Main(string[] args)
         {
-            show first = new show();
-            first.name = "The Big Bang Theory";
-            Console.WriteLine(first.name);
-            first.review = 8.1;
+            show BBT = new show("The Big Bang Theory, Comedie, 12, 279, 8.1, false");
+            BBT.display();
         }
     }
 
     internal class show
     {
+        public show() {}
+
+        public show(string info)
+        {
+            string[] splittedInfo = info.Split(',');
+            name = splittedInfo[0];
+            genre = splittedInfo[1];
+            numberOfSeasons = int.Parse(splittedInfo[2]);
+            numberOfEpisodes = int.Parse(splittedInfo[3]);
+            review = double.Parse(splittedInfo[4]) / 10;
+            review = (review < 1) ? review * 10: review;
+            onGoing = bool.Parse(splittedInfo[5]);
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Nume: " + this.name);
+            Console.WriteLine("Genul: " + this.genre);
+            Console.WriteLine("Numar de sezoane: " + this.numberOfSeasons);
+            Console.WriteLine("Numar de episoade: " + this.numberOfEpisodes);
+            Console.WriteLine("Recenzie: " + this.review);
+            if (onGoing)
+                Console.WriteLine("Inca apar episoade.");
+            else
+                Console.WriteLine("Serialul s-a incheiat.");
+        }
+
         public string name
         { get; set; }
 
         public string genre
         { get; set; }
 
-        public int numberOfEpisodes
+        public int numberOfSeasons
         { get; set; }
 
-        public int numberOfSeasons
+        public int numberOfEpisodes
         { get; set; }
 
         public double review
-        {
-            get { return review; }
-            set
-            {
-                if (value >= 0 && value <= 10)
-                    review = value;
-                else
-                    review = -1;
-            }
-        }
-
-        public bool onGoing
-        { get; set; }
-    }
-
-    internal class show
-    {
-        public string name
-        { get; set; }
-
-        public string genre
-        { get; set; }
-
-        public int numberOfEpisodes
-        { get; set; }
-
-        public int numberOfSeasons
-        { get; set; }
-
-        public float review
-        { get; set; }
+        { get; set;}
 
         public bool onGoing
         { get; set; }
