e906d3ab5b6c8e1fb9972cf429ff019eb9829a5f
doroftei1999 <doroftei1999@yahoo.com>
2021-04-25 15:43:33 +0300

    Am terminat Tema 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tema
 {
@@ -7,7 +8,33 @@ namespace Tema
         private static void Main(string[] args)
         {
             show BBT = new show("The Big Bang Theory, Comedie, 12, 279, 8.1, false");
-            BBT.display();
+            Console.WriteLine(BBT.convertToString());
+            show SOA = new show("Sons of anarchy, Drama, 7, 92, 8.5, false");
+            Console.WriteLine(SOA.convertToString());
+            Console.WriteLine("Serialul mai bine cotat este: \n" + show.compare(BBT, SOA).convertToString());
+            string[,] sortedWords = new string[30, 30];
+            int k = 0;
+            Array.Sort(args);
+            for (int i = 0; i < 26; i++)
+            {
+                k = 0;
+                for (int j = 0; j < args.Length; j++)
+                    if (args[j][0] == i + 65 || args[j][0] == i + 97)
+                    {
+                        sortedWords[i, k++] = args[j];
+                        //Array.Resize(ref sortedWords, k); Nu am reusit sa fac resize.
+                    } ///Memoria e consumata degeaba.
+            }
+            for (int i = 0; i < sortedWords.GetLength(0); i++)
+            {
+                if (String.IsNullOrEmpty(sortedWords[i,0]) == false)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < sortedWords.GetLength(1); j++)
+                        if (String.IsNullOrEmpty(sortedWords[i, j]) == false)
+                            Console.Write(sortedWords[i, j] + ' ');
+                }
+            }
         }
     }
 
@@ -26,6 +53,13 @@ namespace Tema
             review = (review < 1) ? review * 10: review;
             onGoing = bool.Parse(splittedInfo[5]);
         }
+        public static show compare(show a, show b)
+        {
+            if (a.review >= b.review)
+                return a;
+            else
+                return b;
+        }
 
         public void display()
         {
@@ -35,9 +69,16 @@ namespace Tema
             Console.WriteLine("Numar de episoade: " + this.numberOfEpisodes);
             Console.WriteLine("Recenzie: " + this.review);
             if (onGoing)
-                Console.WriteLine("Inca apar episoade.");
+                Console.WriteLine("Inca apar episoade.\n");
             else
-                Console.WriteLine("Serialul s-a incheiat.");
+                Console.WriteLine("Serialul s-a incheiat.\n");
+        }
+        public string convertToString()
+        {
+            string onGoingString = (onGoing) ? "Inca apar episoade.\n" : "Serialul s-a incheiat.\n";
+            return "Nume: " + this.name + "\nGenul: " + this.genre +
+                "\nNumar de sezoane: " + this.numberOfSeasons.ToString() + "\nNumar de episoade: " + 
+                this.numberOfEpisodes.ToString() + "\nRecenzie: " + this.review.ToString() + "\n" + onGoingString ;
         }
 
         public string name
